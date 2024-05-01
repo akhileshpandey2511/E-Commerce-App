a913a48680a44951a011bd381df6383e5ae8f10f
Akhilesh8 Pandey <Akhilesh8.Pandey@ril.com>
2024-05-01 13:59:04 +0530

    changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,23 @@
-import React from 'react'
+import React from 'react';
+import Header from './components/Header';
+import { Container } from 'react-bootstrap';
+import Footer from './components/Footer';
+// import HomeScreen from './screens/HomeScreen';
+import { Outlet } from 'react-router-dom';
+
 
 const App = () => {
   return (
-    <h1>Welcome to our App</h1>
+    <>
+    <Header />
+    <main className='py-3'>
+      <Container>
+        {/* <HomeScreen /> */}
+        <Outlet />
+      </Container>
+    </main>
+    <Footer />
+    </> 
   )
 }
 
